5d00d82aff19c7c7241073ebf074f6c4c9ac449b
Naresh7175 <d.naresh7175@gmail.com>
2025-12-04 11:26:38 +0530

    Updated HeathCheck Controller and added Notes file

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,5 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthCheckController {
+
+  @GetMapping("/healthcheck")
+  public String healthCheck(){
+    return "Application is Working!!";
+  }
 
 }
